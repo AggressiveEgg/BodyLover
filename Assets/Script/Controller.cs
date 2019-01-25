bab96830529a1c7189bfd4f877ec8fd3f2bc0583
jirayu.g@hotmail.com <jirayu.g@hotmail.com>
2019-01-25 22:43:43 +0700

    Start

---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class Controller : MonoBehaviour {
+
+    [Header("Button")]
+   [SerializeField] string jump;
+    [SerializeField]string left;
+   [SerializeField] string right;
+    [SerializeField] string item;
+
+    [SerializeField] PlayerIndex playerIndex;
+
+    GamePadState state;
+    GamePadState prevState;
+
+
+
+    public void CheckGamepadState()
+    {
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+
+    }
+
+    public Controller()
+    {
+        jump = "a";
+        left = "f";
+        right = "g";
+        item = "s";
+    }
+
+    public PlayerIndex PIndex
+    {
+        get { return playerIndex; }
+        set { playerIndex = value; }
+    }
+	
+    public void Jump(System.Action action = null)
+    {
+        if(Input.GetKeyDown(jump) || (state.Buttons.A == ButtonState.Pressed && prevState.Buttons.A == ButtonState.Released))
+        {
+            if(action != null)
+            {
+                action();
+            }
+            else
+            {
+                Debug.Log("No action");
+                return;
+            }
+        }
+
+
+    }
+    public void Left(System.Action action)
+    {
+        if(Input.GetKey(left)||state.DPad.Left == ButtonState.Pressed)
+        {
+            if(action != null)
+            { action(); }
+        }
+        else
+        {
+            Debug.Log("No NoAction");
+            return;
+        }
+    }
+    public void Right(System.Action action)
+    {
+        if (Input.GetKey(right) || state.DPad.Right == ButtonState.Pressed)
+        {
+            if (action != null)
+            { action(); }
+        }
+        else
+        {
+            Debug.Log("No NoAction");
+            return;
+        }
+    }
+
+    public void Item(System.Action action)
+    {
+        if (Input.GetKeyDown(item)||( state.Buttons.X == ButtonState.Pressed && prevState.Buttons.X == ButtonState.Released))
+        {
+            if (action != null)
+            { action(); }
+        }
+        else
+        {
+            Debug.Log("No NoAction");
+            return;
+        }
+    }
+
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+
+public class PlayerController : MonoBehaviour {
+
+
+    
+    Controller control;
+    [Header("Setting")]
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+    [SerializeField] int playerindex;
+
+    
+    [SerializeField] GameObject player;
+
+
+    bool isJump;
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        control = new Controller();
+        rb = GetComponent<Rigidbody>();
+        control.PIndex = (PlayerIndex)playerindex;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        control.CheckGamepadState();
+        ForceToDirection();
+        RotateTarget();
+
+	}
+
+
+    public void ForceToDirection()
+    {
+        control.Jump(() => {
+            if (!isJump)
+            {
+                isJump = true;
+                rb.useGravity = true;
+                // gameObject.transform.LookAt(direction.transform);
+                rb.AddForce(0, jumpForce, 0);
+            }
+        });
+
+        
+    }
+    public void RotateTarget()
+    {
+        if (Mathf.Abs( rb.velocity.x )< 100)
+        {
+            control.Left(() =>
+            {
+                rb.AddForce(-speed, 0, 0);
+            });
+            control.Right(() =>
+            {
+                rb.AddForce(speed, 0, 0);
+            });
+            Debug.Log("Velocity " + rb.velocity.x);
+        }
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "ground"||col.gameObject.tag =="Player")
+        {
+            HitFunction();
+        }
+    }
+    public void HitFunction()
+    {
+        isJump = false;
+    }
+}
