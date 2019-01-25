18fa86c51707211669f93ea1bbc4cd2b05b04e47
Nummon <>
2019-01-26 00:31:11 +0700

    add map level

---
+++ Assets/Script/Nummon/LoadLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour 
+{
+    public static LoadLevel instance;
+	void Start () 
+    {
+        if(instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+	}
+
+    public void OnLoadLevel(int index)
+    {
+        Application.LoadLevel(index);
+    }
+}
