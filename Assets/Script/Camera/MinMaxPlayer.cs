4426dbbb5e29d45b52abc97a93f40d5936a110e4
jirayu.g@hotmail.com <jirayu.g@hotmail.com>
2019-01-26 12:48:05 +0700

    Import Map Model and Player Model

---
+++ Assets/Script/Camera/MinMaxPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinMaxPlayer : MonoBehaviour {
+
+    [SerializeField] Transform pos1;
+    [SerializeField] Transform pos2;
+    [SerializeField] float MaxPos;
+
+    Vector3 calpos;
+    float lastpos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        calpos = Vector3.Lerp(pos1.position, pos2.position, 0.5f);
+        float dif = Mathf.Abs(pos1.position.y - pos2.position.y);
+        float pos = 0;
+        if (dif > 5&& dif <MaxPos)
+        {
+            pos = dif / 2;
+            lastpos = pos;
+        }
+        transform.position = new Vector3(calpos.x,calpos.y,calpos.z-lastpos) ;
+	}
+}
---
+++ Assets/Script/PlayerStat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour {
+
+
+    float maxSpeed = 30;
+    float itemEffect = 1;
+
+    int hp;
+    int maxhp;
+
+    
+
+    public float MaxSpeed
+    {
+        get { return MaxSpeed * itemEffect; }
+    }
+    
+    public void SetItemEffect(float effect)
+    {
+        itemEffect = effect;
+
+
+    }
+
+
+}
